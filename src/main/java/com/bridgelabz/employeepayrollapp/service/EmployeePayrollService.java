454b6348199f20c9a7457856d927eac21ee0c887
mudable <mudableshilpa@gmail.com>
2021-12-24 15:18:28 +0530

    Added : UC3 to store data on the database

--- src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
@@ -8,37 +8,41 @@ import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 
 @Service
 class EmployeePayrollService implements IEmploeePayrollService {
+	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 
 	@Override
 	public List<EmployeePayrollData> getEmployeePayrollData() {
-		List<EmployeePayrollData> empDataList = new ArrayList<>();
-		empDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Sakshi", 5000)));
-		return empDataList;
+
+		return employeePayrollList;
 	}
 
 	@Override
 	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, new EmployeePayrollDTO("Reshma", 40000));
-		return empData;
+
+		return employeePayrollList.get(empId - 1);
 	}
 
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, empPayrollDTO);
+		empData = new EmployeePayrollData(employeePayrollList.size() + 1, empPayrollDTO);
+		employeePayrollList.add(empData);
 		return empData;
 	}
 
 	@Override
 	public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, empPayrollDTO);
+		EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
+		empData.setName(empPayrollDTO.name);
+		empData.setSalary(empPayrollDTO.salary);
+		employeePayrollList.set(empId - 1, empData);
 		return empData;
+
 	}
 
 	@Override
 	public void deleteEmployeePayrollData(int empId) {
+		employeePayrollList.remove(empId);
 
 	}
 }
