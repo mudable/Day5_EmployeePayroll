8bae38214a8bacd538be694caa86d3c1b4be7d7d
mudable <mudableshilpa@gmail.com>
2021-12-27 17:40:29 +0530

    Added : UC9 to validate the name and salary

--- src/main/java/com/bridgelabz/employeepayrollapp/Day5EmployeePayrollAppProgramApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/Day5EmployeePayrollAppProgramApplication.java
@@ -13,6 +13,7 @@ public class Day5EmployeePayrollAppProgramApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Day5EmployeePayrollAppProgramApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
-		log.info("Employee Payroll DB User is {} Environment",context.getEnvironment().getProperty("spring.datasource.username"));
+		log.info("Employee Payroll DB User is {} Environment",
+				context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 }
