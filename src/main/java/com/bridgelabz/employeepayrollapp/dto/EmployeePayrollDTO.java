8631d2f3efa00576c42d6b4490ff7934769b2b34
mudable <mudableshilpa@gmail.com>
2021-12-28 16:45:51 +0530

    Added : UC13 to validate all the variables present in side the DTO class

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -14,22 +16,23 @@ import lombok.ToString;
 @ToString
 public class EmployeePayrollDTO {
 
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name is invalid")
-    public String name;
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name is invalid")
+	public String name;
 
-    @Min(value = 500, message = "Min Wage should be more than 500")
-    public long salary;
+	@Min(value = 500, message = "Min Wage should be more than 500")
+	public long salary;
+	@Pattern(regexp = "male|female", message = " Gender  needs to be male or female")
+	public String gender;
 
-    public String gender;
-
-   @JsonFormat(pattern="dd MMM yyyy")
-    public LocalDate startDate;
-
-    public String note;
-
-    public String profilePic;
-
-    public List<String> departments;
-
+	@JsonFormat(pattern = "dd MMM yyyy")
+	@NotNull(message = "Startdate should not be empty")
+	@PastOrPresent(message = "startDate shuld be past or todays date")
+	public LocalDate startDate;
+	@NotBlank(message = "Note can not be empty")
+	public String note;
+	@NotBlank(message = "profilePic can not be empty")
+	public String profilePic;
+	@NotNull(message = "department should not be empty")
+	public List<String> departments;
 
 }
