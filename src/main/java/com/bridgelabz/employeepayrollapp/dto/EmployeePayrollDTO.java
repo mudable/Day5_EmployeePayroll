3f724eb125028f036b13eeb1a443fe952786f5a2
mudable <mudableshilpa@gmail.com>
2021-12-29 08:33:38 +0530

    Added :UC14 To store data in to mysql db;

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -16,23 +16,24 @@ import lombok.ToString;
 @ToString
 public class EmployeePayrollDTO {
 
-	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name is invalid")
-	public String name;
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee Name is invalid")
+    public String name;
 
-	@Min(value = 500, message = "Min Wage should be more than 500")
-	public long salary;
-	@Pattern(regexp = "male|female", message = " Gender  needs to be male or female")
-	public String gender;
+    @Min(value = 500, message = "Min Wage should be more than 500")
+    public long salary;
+    @Pattern(regexp = "male|female", message = " Gender  needs to be male or female")
+    public String gender;
 
-	@JsonFormat(pattern = "dd MMM yyyy")
-	@NotNull(message = "Startdate should not be empty")
-	@PastOrPresent(message = "startDate shuld be past or todays date")
-	public LocalDate startDate;
-	@NotBlank(message = "Note can not be empty")
-	public String note;
-	@NotBlank(message = "profilePic can not be empty")
-	public String profilePic;
-	@NotNull(message = "department should not be empty")
-	public List<String> departments;
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Startdate should not be empty")
+    @PastOrPresent(message = "startDate shuld be past or todays date")
+    public LocalDate startDate;
+    @NotBlank(message = "Note can not be empty")
+    public String note;
+    @NotBlank(message = "profilePic can not be empty")
+    public String profilePic;
+    @NotNull(message = "department should not be empty")
+    public List<String> departments;
+
 
 }
